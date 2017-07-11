0364fb9cc5e10cc215af13f29875b1b4bd86c5b9
Bianca <bikinavisho@yahoo.com>
2017-07-11 12:03:37 -0500

    Create New Task is now properly formatted in a pleasing manner.

--- src/components/task_new.js
+++ src/components/task_new.js
@@ -6,7 +6,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 class TaskNew extends Component{
-
   constructor(props) {
     super(props);
     this.state = {value: '',
@@ -16,18 +15,6 @@ class TaskNew extends Component{
     this.handleSubmit = this.handleSubmit.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
 }
-
-  renderField(field){
-    return(
-      <div >
-        <label>{field.label}</label>
-        <input
-          type={field.type}
-          {...field.input}
-        />
-      </div>
-    );
-  }
 
   onInputChange(event){
     this.setState({ [event.target.name]: event.target.value });
@@ -47,40 +34,49 @@ class TaskNew extends Component{
   render(){
     const { handleSubmit } = this.props;
     return(
-      <form onSubmit={this.handleSubmit}>
-      <div>
-        <h3>Task title</h3>
-        <Link to="/">Task List</Link>
-      </div>
-        <input
-          label="Title"
-          name="title"
-          type="text"
-          value={this.state.title}
-          onChange={this.onInputChange}
-        />
-        <select
-          name="priority"
-          value={this.state.priority}
-          onChange={this.onInputChange}
-        >
-        <label>Priority</label>
+      <form onSubmit={this.handleSubmit} className="pure-form pure-form-aligned">
+        <fieldset>
+          <legend>
+            <h3>Create A New Task</h3>
+          </legend>
+          <div className="pure-control-group">
+            <label htmlFor="title">Task Name</label>
+            <input
+              name="title"
+              type="text"
+              value={this.state.title}
+              onChange={this.onInputChange}
+            />
+          </div>
 
-          <option value='0'>Low</option>
-          <option value='1'>Medium</option>
-          <option value='2'>High</option>
+          <div className="pure-control-group">
+            <label htmlFor="priority">Priority</label>
+            <select
+              name="priority"
+              value={this.state.priority}
+              onChange={this.onInputChange}
+            >
+              <option value="0">Low</option>
+              <option value="1">Medium</option>
+              <option value="2">High</option>
+            </select>
+          </div>
 
-          </select>
-        <input
-          label="Date Due"
-          name="datedue"
-          type="date"
-          value={this.state.datedue}
-          onChange={this.onInputChange}
-        />
+          <div className="pure-control-group">
+            <label htmlFor="datedue">Date Due</label>
+            <input
+              name="datedue"
+              type="date"
+              value={this.state.datedue}
+              onChange={this.onInputChange}
+            />
+          </div>
 
-        <button className="pure-button right-buffer" type="submit">Submit</button>
-        <Link className= "pure-button cancel-new" to="/">Cancel</Link>
+          <div className="pure-controls">
+            <button className="pure-button pure-button-primary right-buffer" type="submit">Submit</button>
+            <Link className= "pure-button cancel-new" to="/">Cancel</Link>
+          </div>
+        </fieldset>
       </form>
     );
   }
