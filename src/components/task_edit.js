bcd5f2dd0d34c504148066529f4cb57bac8ad55f
Bianca <bikinavisho@yahoo.com>
2017-07-11 09:46:18 -0500

    Updated Task Title page form

--- src/components/task_edit.js
+++ src/components/task_edit.js
@@ -1,48 +1,48 @@
 import React, { Component } from 'react';
-import { Field, reduxForm} from 'redux-form';
 import { Link } from 'react-router-dom';
 
 class TaskEdit extends Component{
-  onSubmit(values){
+  handleSubmit(values){
     console.log(values);
   }
 
-  renderField(field){
-    return(
-      <div >
-        <label>{field.label}</label>
-        <input
-          type={field.type}
-          {...field.input}
-        />
-      </div>
-    );
-  }
-
   render(){
-    const { handleSubmit } = this.props;
     return(
-      <form onSubmit={ handleSubmit(this.onSubmit.bind(this))}>
-        <Field
-          label="Priority"
-          name="priority"
-          type="text"
-          component={this.renderField}
-        />
-        <Field
-          label="Date Due"
-          name="datedue"
-          type="text"
-          component={this.renderField}
-        />
+      <form onSubmit={ this.handleSubmit(this)} className="pure-form pure-form-aligned">
+        <fieldset>
+          <legend>Edit Task</legend>
 
-       <button className="pure-button right-buffer" type="submit">Submit</button>
-        <Link className="pure-button cancel-edit" to="/">Cancel</Link>
+          <div className="pure-control-group">
+            <div>TASK TITLE</div>
+          </div>
+
+          <div className="pure-control-group">
+            <label htmlFor="priority">Priority</label>
+            <select name="priority">
+              <option value="high">High</option>
+              <option value="medium">Medium</option>
+              <option value="low">Low</option>
+            </select>
+
+          </div>
+
+          <div className="pure-control-group">
+            <label htmlFor="datedue">Date Due</label>
+            <input
+              name="priority"
+              type="date"
+            />
+          </div>
+
+          <div className="pure-controls">
+            <button className="pure-button pure-button-primary right-buffer" type="submit">Submit</button>
+             <Link className="pure-button cancel-edit" to="/">Cancel</Link>
+          </div>
+
+        </fieldset>
       </form>
     );
   }
 }
 
-export default reduxForm({
-  form: 'TaskEditForm'
-})(TaskEdit);
+export default TaskEdit;
