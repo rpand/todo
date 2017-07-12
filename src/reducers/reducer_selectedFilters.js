cba91a1e04d0262bff08e12aec6cc1411ba5c75b
Richard Cannoli <rac148@txstate.edu>
2017-07-12 10:05:43 -0500

    Fixed filter reset reducer without Raul's help

--- src/reducers/reducer_selectedFilters.js
+++ src/reducers/reducer_selectedFilters.js
@@ -1,12 +1,14 @@
 import { UPDATE_FILTERS, TOGGLE_COMPLETE, DELETE_TASK } from '../actions';
 
-const selectedFilters = {lowPriority: true, medPriority: true, highPriority: true, todayOnly: false, sortBy: "alphaAsc"};
-
-export default function(state={}, action){
+export default function(state={lowPriority: true, medPriority:
+  true, highPriority: true, todayOnly: false,
+  sortBy: "alphaAsc"},
+  action){
+    
   switch(action.type){
     case UPDATE_FILTERS:
         return {...state, ...action.payload};
     default:
-    	return selectedFilters;
+    	return state;
   }
 }
