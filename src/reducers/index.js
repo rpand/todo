6efc1d2f59c9634743893a880936f176e7d29bfa
Dayna DeBacker <daynadebacker2@gmail.com>
2017-07-17 07:33:57 -0500

    Updated Task New to use Redux Form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import TaskReducer from './reducer_tasks';
 import FilterReducer from './reducer_selectedFilters';
 import AvailableIDReducer from './reducer_availableID';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   tasks: TaskReducer,
   filters: FilterReducer,
-  nextID: AvailableIDReducer
+  nextID: AvailableIDReducer,
+  form: formReducer
 });
 
 export default rootReducer;
