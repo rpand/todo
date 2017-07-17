c9911f14c1a1605bddfce932249cbf3fd6ad5fb5
rpand <>
2017-07-17 16:29:33 -0500

    example it and describe added

---
+++ test/components/task_test.js
@@ -0,0 +1,37 @@
+import { renderComponent, expect } from '../test_helper';
+import Task from '../../src/components/tast';
+
+describe('Task', =>{
+
+  beforeEach(() => {
+});
+
+  it('it displays the due date of the task in MM/DD/YYYY format', =>{
+    expect();
+  });
+
+  it('it displays the priority of the task', =>{
+    expect();
+  });
+
+  it('it displays the title of the task', =>{
+    expect();
+  });
+
+  it('it displays the done status in a checkbox', =>{
+    expect();
+  });
+
+  it('it toggles the done status when the checkbox is clicked', =>{
+    expect();
+  });
+
+  it('it displays a button for editting', =>{
+    expect();
+  });
+
+  it('it displays a button for deleting', =>{
+    expect();
+  });
+
+});
